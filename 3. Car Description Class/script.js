0ee59e59d748db3db5b6f486b5116758d7e1b70e
Manish Chand Barudi <99408291+ManishChand349@users.noreply.github.com>
2023-03-28 18:40:13 +0530

    Add files via upload

---
+++ 3. Car Description Class/script.js
@@ -0,0 +1,13 @@
+class Car {
+  constructor(company, model, year) {
+    this.company = company;
+    this.model = model;
+    this.year = year;
+  }
+
+  getDescription() {
+    return `This is a ${this.year} ${this.company} ${this.model}.`;
+  }
+}
+const myCar = new Car("BMB", "7 Series", 2023);
+console.log(myCar.getDescription()); 
