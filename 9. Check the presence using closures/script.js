4b82c9fd375e93105727b17b256538168e127a8f
Manish Chand Barudi <99408291+ManishChand349@users.noreply.github.com>
2023-03-29 22:12:10 +0530

    Add files via upload

---
+++ 9. Check the presence using closures/script.js
@@ -0,0 +1,10 @@
+function numberChecker(numbers) {
+  return function(num) {
+    return numbers.includes(num);
+  };
+}
+const numbers = [1, 2, 3, 4, 5];
+const isNumberInArray = numberChecker(numbers);
+
+console.log(isNumberInArray(3)); 
+console.log(isNumberInArray(6)); 
